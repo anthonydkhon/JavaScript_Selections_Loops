9932a8585938293aabca92c0e7e09787cbf2e9d4
Anthony Khon <anthony.khon@gmail.com>
2021-06-21 12:11:31 -0400

    Completed exercises 1 & 2

--- app.js
+++ app.js
@@ -3,5 +3,32 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+for (
+    let oddNum = 1; 
+    oddNum <= 100; 
+    oddNum++
+) {
+    if (oddNum % 2 == 1) {
+        console.log(oddNum);
+    } 
+}
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for ( 
+    let i = 1;
+    i <= 100;
+    i++
+    ) 
+    {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz")
+        } else if (i % 5 == 0) {
+            console.log("Fizz")
+        } else if (i % 3 == 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
